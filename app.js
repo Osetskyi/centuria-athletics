ff3e1290433c79b67e14809617d94f37cccabd12
Osetskyi <daniil.osetskyi.23@gmail.com>
2026-08-20 08:22:34 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -6329,3 +6329,5 @@ document.addEventListener("DOMContentLoaded",()=>{
 });
 
 document.addEventListener("DOMContentLoaded",()=>{document.querySelectorAll(".settings-version strong").forEach(el=>el.textContent="v6.25");});
+
+document.addEventListener("DOMContentLoaded",()=>{document.querySelectorAll(".settings-version strong").forEach(el=>el.textContent="v6.26");});
